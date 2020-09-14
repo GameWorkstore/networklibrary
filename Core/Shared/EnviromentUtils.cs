09a30f13d96c6ad6fb8b4a837b3fae2c629e99fb
HanzaRu <john.rosa@aquiris.com.br>
2020-09-13 22:02:51 -0300

    Readme update and Enviroment functions

--- Core/Shared/EnviromentUtils.cs
+++ Core/Shared/EnviromentUtils.cs
@@ -1,58 +1,42 @@
 ﻿using System;
-using System.Collections;
 using System.Linq;
 
-namespace UnityEngine.NetLibrary.Scalability
+namespace UnityEngine.NetLibrary
 {
     public static class EnviromentUtils
     {
-        public const int CENTRAL_PORT = 5000;
-        public const int CENTRAL_MATCHSIZE = 1000;
-        public const int SPAWNER_PORT = 5001;
-        public const int SPAWNER_MATCHSIZE = 1000;
-        public const int DEFAULT_GAME_PORT = 7000;
+        /// <summary>
+        /// Verifies if enviroment argument exists
+        /// </summary>
+        /// <param name="argument">An argument</param>
+        /// <returns>if exists</returns>
+        public static bool Exists(string argument) { return Environment.GetCommandLineArgs().Contains(argument); }
+        
+        /// <summary>
+        /// Format and outputs argument and value for easy process spawn usage.
+        /// </summary>
+        /// <param name="argument">An argument</param>
+        /// <param name="value">An value</param>
+        /// <returns>formatted argument</returns>
+        public static string Format(string argument, string value) { return string.Format(" {0} {1}", argument, value); }
 
-        public const string MATCHMAKING_ARG = "-matchmaking";
-        public const string MATCHID_ARG = "-matchid";
-        public const string PORT_ARG = "-port";
-        public const string MATCHSIZE_ARG = "-matchsize";
-        public const string BOTS_ARG = "-botsize";
-        //public const string CENTRAL_IP = "-ipcentral";
-        public const string SPAWNER_IP = "-ipspawner";
-
-        public const string StandardBatchModeArgs = MATCHMAKING_ARG + " -batchmode -nographics -logfile";
-        public const string StandardtArgs = MATCHMAKING_ARG +" -logfile -screen-width 320 -screen-height 240";
-
-        public static bool ArgProvided(string arg) { return Environment.GetCommandLineArgs().Contains(arg); }
-
-        public static string ArgsFormat(string arg, string obj) { return string.Format(" {0} {1}", arg, obj); }
-
-        public static bool GetArgProvided(string arg, out string value)
+        /// <summary>
+        /// Verifies and returns if argument exists and it's value
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <param name="value">output value of argument</param>
+        /// <returns>if exists and value</returns>
+        public static bool GetValue(string argument, out string value)
         {
             value = string.Empty;
             string[] vars = Environment.GetCommandLineArgs();
-            int index = Array.IndexOf(vars, arg);
+            int index = Array.IndexOf(vars, argument);
             if(index >= 0 && (index + 1) < vars.Length)
             {
                 value = vars[index + 1];
                 return true;
             }
             return false;
-        }
-
-        public static IEnumerator SendRequest(string uri, WWWForm form, Action<bool, string> result)
-        {
-            var www = new WWW(uri, form);
-            yield return www;
-
-            if (string.IsNullOrEmpty(www.error))
-            {
-                result(true, www.text);
-            }
-            else
-            {
-                result(false, www.error);
-            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# networklibrary
-Network Library for Unity Projects based on old UNET!
+# Network Library
+
+Network Library for Unity projects based on old UNET! Transport layer will be updated for better one when unity drops support for LLAPI, or earlier.
+Use it your own risk!
+
+# How to install
+
+At package.json, add these 2 lines of code:
+> "com.gameworkstore.networklibrary": "https://github.com/GameWorkstore/networklibrary.git"
+
+> "com.gameworkstore.patterns": "https://github.com/GameWorkstore/patterns.git"
+
+and wait unity download and compile the package.
+
+Is interesting to write a editor script to update them when necessary!
+
+```csharp
+using UnityEditor;
+using UnityEditor.PackageManager;
+
+public class PackageUpdater
+{
+    [MenuItem("Help/UpdateGitPackages")]
+    public static void TrackPackages()
+    {
+        Client.Add("https://github.com/GameWorkstore/patterns.git");
+        Client.Add("https://github.com/GameWorkstore/networklibrary.git");
+    }
+}
+```
